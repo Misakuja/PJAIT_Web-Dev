77735612500b427fe9b6ab04b2869caaa83cfec6
misakuja <aniatu13@gmail.com>
2025-05-22 09:04:02 +0200

    LAB12 Complete

---
+++ html/LAB12/LAB12_Ex01.js
@@ -0,0 +1,5 @@
+const imie = process.argv[2];
+
+if (imie)
+    console.log(`hello, ${imie}!`);
+
---
+++ html/LAB12/LAB12_Ex02.js
@@ -0,0 +1,38 @@
+let fs = require('fs');
+
+const command = process.argv[2];
+const fileName = process.argv[3];
+const newText = process.argv.slice(4).join(' ');
+
+const folderPath = './files/';
+
+switch (command) {
+    case 'open':
+        fs.readFile(folderPath + fileName, 'utf8', (err, data) => {
+            if (err) {
+                console.error('Error reading file:', err);
+                return;
+            }
+            console.log('File contents:', data);
+        })
+        break;
+    case 'append':
+        fs.appendFile(folderPath + fileName, newText, function (err) {
+            if (err) {
+                console.error('Error reading file:', err);
+                return;
+            }
+            console.log('Updated!');
+        });
+        break;
+    case 'delete':
+        fs.unlink(folderPath + fileName, function (err) {
+            if (err) {
+                console.error('Error reading file:', err);
+                return;
+            }
+            console.log('File deleted!');
+        });
+        break;
+
+}
